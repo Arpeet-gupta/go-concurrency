bde22bd9e512af4e46a4f293143fa033d1f26314
Arpeet-gupta <agarpitgupta96@gmail.com>
2021-03-21 12:15:00 +0000

    Enhancement v1.1.0: Anonymous goroutines with Clousre functions

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Arpeet-gupta/go-concurrency
+module github.com/Arpeet-gupta/go-concurrency/v2
 
 go 1.15
--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("main execution started")
-
+	c := make(chan string)
 	//creating go-routines
 	go func() {
-		fmt.Println("running inside go routine")
+		fmt.Println("hello " + <-c + " from anonymous goroutine")
 	}()
 
-	time.Sleep(10 * time.Millisecond)
+	c <- "Arpeet"
+	// time.Sleep(10 * time.Millisecond)
 	fmt.Println("main execution stopped")
 }
